3e8eabeff5e62e1e971b109e216b7f90a20eca79
NilsOveTen <nils.ove.tendenes@acando.no>
2019-12-17 13:56:02 +0100

    chore: copy of search-api from fdk

---
+++ src/main/java/no/fdk/searchapi/controller/ControllerExceptionHandler.java
@@ -0,0 +1,8 @@
+package no.fdk.searchapi.controller;
+
+import no.fdk.webutils.exceptions.GlobalControllerExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends GlobalControllerExceptionHandler {
+}
